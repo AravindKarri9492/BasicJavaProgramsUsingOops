d6c3c3693c3301e880b7ff2a34afeff34af88e30
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-27 00:05:58 +0530

    ADDED: leap year logic developed

--- src/com/bridgelabz/JavaPrograms.java
+++ src/com/bridgelabz/JavaPrograms.java
@@ -25,4 +25,27 @@ class JavaPrograms
             System.out.println("Tail Percentage "+tailPercentage);
         }
     }
+    //LeapYear
+    public static void leapYear(int yearToCheck)
+    {
+        if(Integer.toString(yearToCheck).length() == 4)  //Ensure year length is 4
+        {
+            if(yearToCheck%4 == 0 && yearToCheck%100 != 0)
+            {
+                System.out.println(+yearToCheck+ " is leap year");
+            }
+            else if(yearToCheck%100 == 0 && yearToCheck%400 == 0)
+            {
+                System.out.println(+yearToCheck+" is leap year");
+            }
+            else
+            {
+                System.out.println(+yearToCheck+ " is not a leap year");
+            }
+        }
+        else
+        {
+            System.err.println("Entered year is not valid");
+        }
+    }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -7,10 +7,32 @@ public class Main
     public static void main(String[] args)
     {
         JavaPrograms object = new JavaPrograms();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter how many times do you want to flip coin : ");
-        int numOfFlips = scanner.nextInt();
-        scanner.close();
-        object.flip(numOfFlips);
+        while(true)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please select which program do you want to run");
+            System.out.println("1.flip coin \n" +
+                    "2.leap year \n" +
+                    "3.exit ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice)
+            {
+                case 1:
+                    System.out.println("Enter how many times do you want to flip coin : ");
+                    int numOfFlips = scanner.nextInt();
+                    object.flip(numOfFlips);
+                    break;
+
+                case 2:
+                    System.out.println("Enter a year to check leap year or not");
+                    int yearToCheck = scanner.nextInt();
+                    object.leapYear(yearToCheck);
+                    break;
+
+                case 3:
+                    System.exit(0);
+            }
+
+        }
     }
 }
