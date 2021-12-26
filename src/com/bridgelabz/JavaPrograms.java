90d58367930d39c2fa0e59e7c6edb7abbad09c22
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-26 23:59:24 +0530

    ADDED: code to flip coin and print percentage of heads and tails

--- src/com/bridgelabz/JavaPrograms.java
+++ src/com/bridgelabz/JavaPrograms.java
@@ -2,5 +2,27 @@ package com.bridgelabz;
 
 class JavaPrograms
 {
-
+    //FlipCoin
+    public static void flip(int numOfFlips)
+    {
+        if(numOfFlips <= 0 )
+        {
+            System.err.println("Enter a positive number");
+        }
+        else
+        {
+            int headCount = 0;
+            for(int i=1;i<=numOfFlips;i++)
+            {
+                if(Math.random() < 0.5)
+                {
+                    headCount++;
+                }
+            }
+            double headPercentage = (headCount * 100)/numOfFlips;
+            double tailPercentage = (100 - headPercentage);
+            System.out.println("Head Percentage "+headPercentage);
+            System.out.println("Tail Percentage "+tailPercentage);
+        }
+    }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,6 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        JavaPrograms object = new JavaPrograms();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many times do you want to flip coin : ");
+        int numOfFlips = scanner.nextInt();
+        scanner.close();
+        object.flip(numOfFlips);
+    }
 }
